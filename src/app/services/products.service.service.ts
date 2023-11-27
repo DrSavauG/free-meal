bf5f05e221f4129ac098344043e475d135cf5c4e
DrSavauG <46158444+DrSavauG@users.noreply.github.com>
2023-11-27 10:20:05 +0300

    feat: upgrade httpservice

--- src/app/services/products.service.service.ts
+++ src/app/services/products.service.service.ts
@@ -2,17 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Product, Products } from "../models/mock-products";
 import { map, Observable } from "rxjs";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
+  private readonly apiUrl:string = environment.apiUrl;
+  private readonly apiUrlRandom:string = environment.apiUrlRandom;
+
   constructor(private http: HttpClient) {
   }
 
-  getData(url: string): Observable<Product[]> {
-    return this.http.get<Products>(url).pipe(map((response) => response.meals));
+  getData(): Observable<Product[]> {
+    return this.http.get<Products>(this.apiUrlRandom).pipe(map((response) => response.meals));
   }
 }
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment: { production: boolean, apiUrl: string, apiUrlRandom: string } = {
+  production: true,
+  apiUrl: 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=53024',
+  apiUrlRandom: 'https://www.themealdb.com/api/json/v1/1/random.php'
+  // apiUrl// www.themealdb.com/api/json/v1/1/search.php?f=a
+};

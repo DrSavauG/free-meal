daef2416f8f5edefbaeb73cf1e2f810473b88191
DrSavauG <dr.savaug@gmail.com>
2023-11-30 09:25:00 +0300

    feat: add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,12 @@ import { HttpClientModule } from "@angular/common/http";
 import { HttpService } from "./services/products.service";
 import { Product } from "./models/mock-products";
 import { Observable } from "rxjs";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, BodyComponent, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, BodyComponent, HttpClientModule, FooterComponent],
   providers: [HttpService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'

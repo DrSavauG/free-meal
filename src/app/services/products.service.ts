4db20298c119413209768f888fd741a68bfb2810
DrSavauG <dr.savaug@gmail.com>
2024-02-05 11:52:24 +0300

    refactor: rm handleResponse

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
-import { map, Observable, tap } from "rxjs";
+import { map, Observable } from "rxjs";
 
 import { Product, Products } from "../models/mock-products";
 import { environment } from "../../environments/environment";
@@ -20,30 +20,24 @@ export class HttpService {
 
   public getRandomData(): Observable<Product[]> {
     return this.http.get<Products>(this.apiUrlRandom).pipe(
-      map((response) => response.meals),
-      tap((response) => this.handleResponse(response))
+      map((response) => response.meals)
     );
   }
 
   public getSearchData(idMeals: string): Observable<Product[]> {
     const getUrl: string = `${this.apiItemIdUrl}${idMeals}`;
     return this.http.get<Products>(getUrl).pipe(
-      map((response) => response.meals),
-      tap((response) => this.handleResponse(response))
+      map((response) => response.meals)
     );
   }
 
   public searchByLetter(letter: string): Observable<Product[]> {
     const searchLetterUrl: string = `${this.apiItemsLetterUrl}${letter}`;
     return this.http.get<Products>(searchLetterUrl).pipe(
-      map((response) => response.meals),
-      tap((response) => this.handleResponse(response))
+      map((response) => response.meals)
     );
   }
 
-  private handleResponse(response: Product[]): void {
-    response;
-  }
 }
 
 export class ProductsService {

d83dc08a4c877d1a10a0b204cb604be003a9abfe
DrSavauG <dr.savaug@gmail.com>
2024-02-12 16:20:59 +0300

    feat: add links

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
 import { TitleCasePipe } from "@angular/common";
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import { RouterLink, Router } from "@angular/router";
 
 import { HttpService } from "../../services/products.service";
+
 
 @Component({
   selector: 'header-component',
@@ -12,18 +14,26 @@ import { HttpService } from "../../services/products.service";
   imports: [
     TitleCasePipe,
     FormsModule,
-    ReactiveFormsModule
-],
+    ReactiveFormsModule,
+    RouterLink
+  ],
 })
 
 export class HeaderComponent {
   constructor(
-    private httpService: HttpService
+    private httpService: HttpService,
+    private router: Router
   ) {
   }
 
   public header: string = 'free meal';
   public searchControl: FormControl<string | null> = new FormControl<string>('', [Validators.required, Validators.minLength(1)]);
+
+  toMainPageAndReload() {
+    this.router.navigate(['/']).then(() => {
+      location.reload();
+    });
+  }
 
   // public onSearch(): void {
   //   const searchUserInput:string|null = this.searchControl.value;

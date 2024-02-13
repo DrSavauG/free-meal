2016289081b218f67176ba57ffff6befcbdbdfd3
DrSavauG <dr.savaug@gmail.com>
2024-02-13 12:29:02 +0300

    feat: add search by letter

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -35,13 +35,27 @@ export class HeaderComponent {
     });
   }
 
-  // public onSearch(): void {
-  //   const searchUserInput:string|null = this.searchControl.value;
-  //   if(searchUserInput) {
-  //     this.httpService.getSearchData(searchUserInput).subscribe((items) => {
-  //       console.log(items);
-  //     });
+  protected onSearch(): void {
+    const searchUserInput: string | null = this.searchControl.value;
+    if(searchUserInput) {
+      this.httpService.getSearchData(searchUserInput).subscribe((items) => {
+        console.log(items);
+      });
+    }
+  }
+  searchByLetter(event: Event): void {
+    const {value} = event.target as HTMLInputElement;
+    console.log(value);
+      if (value) {
+        this.httpService.getSearchByName(value).subscribe((items)=>
+        console.log(items));
+  //     this.categorias = this.categoriaArmazenadas.filter((categoria) =>
+  //       categoria.strCategory.toLowerCase().includes(value)
+  //     )
+    }
+        //   else {
+  //     this.categorias = [...this.categoriaArmazenadas]
   //   }
-  // }
+  }
 }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiItemIdUrl: 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=',
   apiItemsLetterUrl: 'https://www.themealdb.com/api/json/v1/1/search.php?f=',
   apiUrlRandom: 'https://www.themealdb.com/api/json/v1/1/random.php',
-  URL:'https://www.themealdb.com/api/json/v1/1/search.php?s=',
+  apiSearchByNameUrl:'https://www.themealdb.com/api/json/v1/1/search.php?s=',
   urlImageIngredient:'https://www.themealdb.com/images/ingredients/'
 };
